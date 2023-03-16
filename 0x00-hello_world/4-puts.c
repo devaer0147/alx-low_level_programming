95dd30186967314b04abb5212e0b8916a945cbec
rotm <rofkid@gmail.com>
2023-03-16 14:57:05 -0700

    c is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
 /* function that prints out with puts */
 int main(void)
 {
+	/* using the puts function */
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
