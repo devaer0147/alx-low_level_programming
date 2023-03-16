a0280101eaf47c8da0902b5a2b470f4f02234a9d
rotm <rofkid@gmail.com>
2023-03-16 16:16:38 -0700

    betty mapaminah

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printing using printf function
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
