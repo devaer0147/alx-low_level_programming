be4fcb63b48c68960946813f0a8bffc91ddd32e2
rotm <rofkid@gmail.com>
2023-03-17 19:42:25 -0700

    fixed indentations

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,26 +6,26 @@
 /**
 * main - Entry point
 * if else statement for +, -, ==
-* return: Always 0 (success)
+* Return: Always 0 (success);
 */
-int main()
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-    if(n > 0)
-    {
-    	printf("%i is positive\n", n);
-    }
-    else if(n == 0)
-    {
-    	printf("%i is zero\n", n);
-    } 
-    else if(n < 0)
-    {
-    	printf("%i is negative\n", n);
-    }
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+    		printf("%i is zero\n", n);
+	}
+	else if (n < 0)
+	{
+    		printf("%i is negative\n", n);
+	}
 	return (0);
 }
