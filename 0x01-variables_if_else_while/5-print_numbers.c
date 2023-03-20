7c90e3f9472006cf611c9167dea99058206b92b0
rotm <rofkid@gmail.com>
2023-03-20 13:10:36 -0700

    printing number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 number
+ * Return: Always 0 (Succes);
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%i", n);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 number
+ * Return: Always 0 (Succes);
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
