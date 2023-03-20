da149be93d3270992d35f0acd601e4fd74b4444a
rotm <rofkid@gmail.com>
2023-03-20 12:06:46 -0700

    printing alphABETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,16 @@
 int main(void)
 {
 	char a;
+	char A;
 
 	for (a = 'a'; a <= 'z'; a++)
 	{
 		putchar(a);
 	}
+	for (A = 'A' ; A <= 'Z' ; A++)
+	{
+		putchar(A);
+	}
 	putchar('\n');
+	return (0);
 }
