30295bbf23edde993c4bec1e8d3bf88b006f2590
rotm <rofkid@gmail.com>
2023-03-23 04:47:57 -0700

    fix betty styl doc issue

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,17 @@
  * Return: 0;
  */
 void _putchar(char p);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
 void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ */
+
 void print_alphabet_x10(void);
 
 #endif
